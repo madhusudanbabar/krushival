b8033a929b7a9a8e295617b0ad42ba24acd154cc
madhusudan babar <madhusudanbabar@gmail.com>
2020-04-16 11:20:49 +0530

    heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+  apiKey: "AIzaSyCUq4iqHKzckYm3PQ1_YrvkkUYq_Nv-dms",
+  authDomain: "ieee-krushival.firebaseapp.com",
+  databaseURL: "https://ieee-krushival.firebaseio.com",
+  projectId: "ieee-krushival",
+  storageBucket: "ieee-krushival.appspot.com",
+  messagingSenderId: "892909807247",
+  appId: "1:892909807247:web:b7ea76eae854484fdf72fa"
+}
 };
 
 /*
