1ac42c47ab4c02e09cc789ead057bbd76e4a81f8
madhusudan babar <madhusudanbabar@gmail.com>
2020-04-17 09:48:47 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,12 @@
 export const environment = {
   production: true,
-  export const environment = {
-    production: false,
-    firebaseConfig: {
-    apiKey: "AIzaSyCUq4iqHKzckYm3PQ1_YrvkkUYq_Nv-dms",
-    authDomain: "ieee-krushival.firebaseapp.com",
-    databaseURL: "https://ieee-krushival.firebaseio.com",
-    projectId: "ieee-krushival",
-    storageBucket: "ieee-krushival.appspot.com",
-    messagingSenderId: "892909807247",
-    appId: "1:892909807247:web:b7ea76eae854484fdf72fa"
+  firebaseConfig: {
+  apiKey: "AIzaSyCUq4iqHKzckYm3PQ1_YrvkkUYq_Nv-dms",
+  authDomain: "ieee-krushival.firebaseapp.com",
+  databaseURL: "https://ieee-krushival.firebaseio.com",
+  projectId: "ieee-krushival",
+  storageBucket: "ieee-krushival.appspot.com",
+  messagingSenderId: "892909807247",
+  appId: "1:892909807247:web:b7ea76eae854484fdf72fa"
   }
-};
+}
